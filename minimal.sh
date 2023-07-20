d315f74b319387ec4635b2386a89d59e5b535b7c
sulincix <parduscix@yandex.ru>
2023-07-20 14:28:44 +0000

    Update minimal.sh

--- minimal.sh
+++ minimal.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# enable live
+wget https://gitlab.com/turkman/devel/assets/misc/-/raw/main/live-config.initd -O /etc/init.d/live-config
+chmod 755 /etc/init.d/live-config
+rc-update add live-config
 exit 0
