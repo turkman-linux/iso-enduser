f30dfe1ff054371aed5062a6a55f0249a6189d31
sulincix <parduscix@yandex.ru>
2023-07-20 14:43:41 +0000

    Update minimal.sh

--- minimal.sh
+++ minimal.sh
@@ -3,4 +3,5 @@
 wget https://gitlab.com/turkman/devel/assets/misc/-/raw/main/live-config.initd -O /etc/init.d/live-config
 chmod 755 /etc/init.d/live-config
 rc-update add live-config
+rc-update add udhcpc
 exit 0
