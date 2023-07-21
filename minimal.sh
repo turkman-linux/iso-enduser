f3945b2e31b666479c08eba0d3101ebe773788b9
sulincix <parduscix@yandex.ru>
2023-07-21 06:41:14 +0000

    Update minimal.sh

--- minimal.sh
+++ minimal.sh
@@ -7,6 +7,7 @@ chmod 755 /etc/init.d/live-config
 echo "tmpfs /tmp tmpfs rw 0 0" > /etc/fstab
 ln -s /proc/mounts /etc/mtab
 sed -i "s|#agetty_options.*|agetty_options=\" -l /usr/bin/login\"|" /etc/conf.d/agetty
+chmod u+s /bin/su /usr/bin/su
 rc-update add live-config
 rc-update add udhcpc
 exit 0
