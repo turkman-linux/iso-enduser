cb165ff3d28915c725159844240bb8be7a78a8c8
sulincix <parduscix@yandex.ru>
2023-08-01 06:48:24 +0000

    Update minimal.sh

--- minimal.sh
+++ minimal.sh
@@ -4,7 +4,9 @@ ymp repo --update --ignore-gpg
 ymp it shadow audit --no-emerge
 ymp it e2fsprogs dialog rsync grub parted --no-emerge
 wget https://gitlab.com/turkman/devel/sources/live-boot/-/raw/master/live-config.initd -O /etc/init.d/live-config
+wget https://gitlab.com/turkman/devel/sources/live-boot/-/raw/master/live-config.sh -O /usrlibexec/live-config
 chmod 755 /etc/init.d/live-config
+chmod 755 /usr/libexec/live-config
 echo "tmpfs /tmp tmpfs rw 0 0" > /etc/fstab
 ln -s /proc/mounts /etc/mtab
 sed -i "s|#agetty_options.*|agetty_options=\" -l /usr/bin/login\"|" /etc/conf.d/agetty
@@ -12,4 +14,9 @@ chmod u+s /bin/su /usr/bin/su
 rc-update add live-config
 rc-update add udhcpc
 rc-update add hostname
+mkdir -p /lib64/locale
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+echo "" >> /etc/locale.gen
+locale-gen
+sed -i "s/C/en_US/g" /etc/profile.d/locale.sh
 exit 0
