4d79bf1d24ff7c39c040720b11b75bb1c5110a05
sulincix <parduscix@yandex.ru>
2023-08-07 07:57:41 +0000

    Update cinnamon-enduser.sh

--- cinnamon-enduser.sh
+++ cinnamon-enduser.sh
@@ -27,7 +27,7 @@ sed -i "s/C/en_US/g" /etc/profile.d/locale.sh
 ymp repo --update --allow-oem --ignore-gpg
 ymp it xinit xorg-server xterm freetype xauth xkbcomp xkeyboard-config @x11.drivers --no-emerge --allow-oem
 ymp it elogind shadow pipewire wireplumber libtool firefox-installer mousepad gpicview fuse fuse2 --no-emerge --allow-oem
-ymp it @cinnamon caribou dejavu adwaita-icon-theme gsettings-desktop-schemas polkit-gnome elementary-appcenter --no-emerge --allow-oem
+ymp it @cinnamon caribou dejavu adwaita-icon-theme gsettings-desktop-schemas polkit-gnome elementary-appcenter pavucontrol gnome-terminal --no-emerge --allow-oem
 chmod u+s /usr/libexec/polkit-gnome-authentication-agent-1 /usr/bin/pkexec /usr/lib64/polkit-1/polkit-agent-helper-1
 # install wifi and bluetooth
 ymp it wpa_supplicant networkmanager bluez --no-emerge --allow-oem
