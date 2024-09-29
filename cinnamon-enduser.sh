1c46ffe9e0163d730f63319340c35a0d5b24c0be
sulincix <parduscix@yandex.ru>
2024-09-29 20:19:39 +0000

    add pipewire service

--- cinnamon-enduser.sh
+++ cinnamon-enduser.sh
@@ -50,6 +50,7 @@ rc-update add lightdm
 rc-update add bluetooth
 rc-update add polkit
 rc-update add touchegg
+rc-update add pipewire
 ymp clean --allow-oem
 # revert hardened bindir
 mkdir -p /usr/local/bin
